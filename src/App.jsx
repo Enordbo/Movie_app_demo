36cfef2eff53afa7e5c5a89e0d58b209028d3c3b
Enordbo <eirik.nordbo@outlook.com>
2025-05-20 22:55:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { Movieprovider } from "./contexts/MovieContexts";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Movieprovider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+    </Movieprovider>
+  );
+}
+
+export default App;
